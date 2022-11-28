8bc4a145b996909f3e41e121fd16808bc6353137
daviti <davit.amiranidze@gmail.com>
2022-11-28 21:04:47 +0400

    first commit

---
+++ app/src/main/java/com/example/fragments/fragments/DashboardFragment.kt
@@ -0,0 +1,37 @@
+package com.example.fragments.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
+import com.example.fragments.R
+
+
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+
+    private lateinit var  editText: EditText
+    private lateinit var button: Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        editText = view.findViewById(R.id.editText)
+        button = view.findViewById(R.id.button)
+
+        button.setOnClickListener{
+
+            val amount = editText.text.toString().toInt()
+
+            val action = DashboardFragmentDirections.actionDashboardFragment3ToNotificationFragment2(amount)
+
+
+            findNavController().navigate(action)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/fragments/fragments/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.fragments.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragments.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+}
